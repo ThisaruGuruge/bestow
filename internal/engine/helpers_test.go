9d85d19a875e0ccbb2cba3235136820c1ee22321
ThisaruG <thisaru.guruge@gmail.com>
2026-06-30 23:43:13 +0530

    test: add tests for package operations

---
+++ internal/engine/helpers_test.go
@@ -0,0 +1,147 @@
+/*
+All Rights Reversed (ɔ)
+*/
+
+package engine
+
+import (
+	"errors"
+	"io"
+	"log/slog"
+	"testing"
+
+	"github.com/redpierrot/bestow/internal/file"
+)
+
+func newTestEngine(src, dest string, fs *MockFileSystem, ignoreList *IgnoreList) *Engine {
+	logger := slog.New(slog.NewTextHandler(io.Discard, nil))
+	if ignoreList == nil {
+		ignoreList = newTestIgnoreList(fs, logger, nil)
+	}
+	return &Engine{
+		logger:      logger,
+		source:      src,
+		destination: dest,
+		fileSystem:  fs,
+		ignore:      ignoreList,
+	}
+}
+
+func newTestIgnoreList(fs *MockFileSystem, logger *slog.Logger, items []string) *IgnoreList {
+	if items == nil {
+		items = make([]string, 0)
+	}
+	return &IgnoreList{
+		src:          "",
+		items:        items,
+		reader:       fs,
+		logger:       logger,
+		packageLists: make(map[string][]string),
+	}
+}
+
+func validateErrScenario(t *testing.T, wantErr bool, err, wantErrIs error) bool {
+	t.Helper()
+	if (err != nil) != wantErr {
+		t.Fatalf("got error %v, want %v", err, wantErr)
+	}
+	if wantErr && !errors.Is(err, wantErrIs) {
+		t.Fatalf("error got %v, want %v", err, wantErrIs)
+	}
+	return wantErr
+}
+
+type MockFileSystem struct {
+	listDirFn          func(parent string) ([]string, error)
+	listAllFilesFn     func(parent string) ([]string, error)
+	createFileFn       func(path, content string) error
+	createDirFn        func(path string) error
+	linkFn             func(src, target string) error
+	moveFn             func(src, target string) error
+	removeFn           func(path string) error
+	isDirFn            func(path string) (bool, error)
+	existsFn           func(path string) (bool, error)
+	readLinesFn        func(path string) ([]string, error)
+	existingFileTypeFn func(src, dest string) (file.ExistingType, error)
+}
+
+func (mf *MockFileSystem) ListDirs(parent string) ([]string, error) {
+	if mf.listDirFn != nil {
+		return mf.listDirFn(parent)
+	}
+	return nil, nil
+}
+
+func (mf *MockFileSystem) ListAllFiles(parent string) ([]string, error) {
+	if mf.listAllFilesFn != nil {
+		return mf.listAllFilesFn(parent)
+	}
+	return nil, nil
+}
+
+func (mf *MockFileSystem) CreateFile(path, content string) error {
+	if mf.createFileFn != nil {
+		return mf.createFileFn(path, content)
+	}
+	return nil
+}
+
+func (mf *MockFileSystem) CreateDir(path string) error {
+	if mf.createDirFn != nil {
+		return mf.createDirFn(path)
+	}
+	return nil
+}
+
+func (mf *MockFileSystem) Link(src, target string) error {
+	if mf.linkFn != nil {
+		return mf.linkFn(src, target)
+	}
+	return nil
+}
+
+func (mf *MockFileSystem) Move(src, target string) error {
+	if mf.moveFn != nil {
+		return mf.moveFn(src, target)
+	}
+	return nil
+}
+
+func (mf *MockFileSystem) Remove(path string) error {
+	if mf.removeFn != nil {
+		return mf.removeFn(path)
+	}
+	return nil
+}
+
+func (mf *MockFileSystem) IsDir(path string) (bool, error) {
+	if mf.isDirFn != nil {
+		return mf.isDirFn(path)
+	}
+	return false, nil
+}
+
+func (mf *MockFileSystem) IsEmptyDir(path string) (bool, error) {
+	return true, nil
+}
+
+func (mf *MockFileSystem) Exists(path string) (bool, error) {
+	if mf.existsFn != nil {
+		return mf.existsFn(path)
+	}
+	return false, nil
+}
+
+func (mf *MockFileSystem) ReadLines(path string) ([]string, error) {
+	if mf.readLinesFn != nil {
+		return mf.readLinesFn(path)
+	}
+	return make([]string, 0), nil
+}
+
+func (mf *MockFileSystem) ExistingFileType(src, dest string) (file.ExistingType, error) {
+	if mf.existingFileTypeFn != nil {
+		return mf.existingFileTypeFn(src, dest)
+	}
+	return file.ExistingUnknown, nil
+}
---
+++ internal/engine/package_operations_test.go
@@ -0,0 +1,190 @@
+/*
+All Rights Reversed (ɔ)
+*/
+
+package engine
+
+import (
+	"io"
+	"log/slog"
+	"os"
+	"path/filepath"
+	"slices"
+	"testing"
+)
+
+func TestPackageOperations_buildPackageList(t *testing.T) {
+	tests := []struct {
+		name  string
+		setup func()
+		args  []string
+	}{}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+		})
+	}
+}
+
+func TestPackageOperations_retrieveAllPackages(t *testing.T) {
+	tests := []struct {
+		name      string
+		setup     func() *Engine
+		want      []string
+		wantErr   bool
+		wantErrIs error
+	}{
+		{
+			name: "retrieve all packages",
+			setup: func() *Engine {
+				src := filepath.Join("home", "user", "dotfiles")
+				dest := filepath.Join("home", "user")
+				dirs := []string{filepath.Join(src, "pkg1"), filepath.Join(src, "pkg2"), filepath.Join(src, "pkg3")}
+				fs := &MockFileSystem{
+					listDirFn: func(parent string) ([]string, error) {
+						return dirs, nil
+					},
+				}
+				return newTestEngine(src, dest, fs, nil)
+			},
+			want: []string{"pkg1", "pkg2", "pkg3"},
+		},
+		{
+			name: "retrieve all packages - list dir error",
+			setup: func() *Engine {
+				fs := &MockFileSystem{
+					listDirFn: func(parent string) ([]string, error) {
+						return nil, os.ErrNotExist
+					},
+				}
+				return newTestEngine("", "", fs, nil)
+			},
+			wantErr:   true,
+			wantErrIs: os.ErrNotExist,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			engine := tc.setup()
+			pkgCandidates, err := engine.retrieveAllPackages()
+			if validateErrScenario(t, tc.wantErr, err, tc.wantErrIs) {
+				return
+			}
+			if !slices.Equal(pkgCandidates, tc.want) {
+				t.Fatalf("got %v, want %v", pkgCandidates, tc.want)
+			}
+		})
+	}
+}
+
+func TestPackageOperations_retrievePackagesFromArgs(t *testing.T) {
+	tests := []struct {
+		name      string
+		setup     func() *Engine
+		args      []string
+		want      []string
+		wantErr   bool
+		wantErrIs error
+	}{
+		{
+			name: "retrieve packages from args",
+			setup: func() *Engine {
+				fs := &MockFileSystem{
+					isDirFn: func(path string) (bool, error) {
+						return true, nil
+					},
+				}
+				return newTestEngine("", "", fs, nil)
+			},
+			want: []string{"pkg1", "pkg2", "pkg3"},
+			args: []string{"pkg1", "pkg2", "pkg3"},
+		},
+		{
+			name: "retrieve packages from args - root package",
+			setup: func() *Engine {
+				fs := &MockFileSystem{
+					isDirFn: func(path string) (bool, error) {
+						return true, nil
+					},
+				}
+				return newTestEngine("", "", fs, nil)
+			},
+			args:      []string{"pkg1", ".", "pkg3"},
+			wantErr:   true,
+			wantErrIs: ErrRootIsNotPkg,
+		},
+		{
+			name: "retrieve packages from args - not dir",
+			setup: func() *Engine {
+				fs := &MockFileSystem{
+					isDirFn: func(path string) (bool, error) {
+						return false, nil
+					},
+				}
+				return newTestEngine("", "", fs, nil)
+			},
+			args:      []string{"pkg1", "pkg3"},
+			wantErr:   true,
+			wantErrIs: ErrPkgIsNotDir,
+		},
+		{
+			name: "retrieve packages from args - dir checking fail",
+			setup: func() *Engine {
+				fs := &MockFileSystem{
+					isDirFn: func(path string) (bool, error) {
+						return false, os.ErrPermission
+					},
+				}
+				return newTestEngine("", "", fs, nil)
+			},
+			args:      []string{"pkg1", "pkg3"},
+			wantErr:   true,
+			wantErrIs: os.ErrPermission,
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			engine := tc.setup()
+			pkgCandidates, err := engine.retrievePackagesFromArgs(tc.args)
+			if validateErrScenario(t, tc.wantErr, err, tc.wantErrIs) {
+				return
+			}
+			if !slices.Equal(pkgCandidates, tc.want) {
+				t.Fatalf("got %v, want %v", pkgCandidates, tc.want)
+			}
+		})
+	}
+}
+
+func TestPackageOperations_filterPackages(t *testing.T) {
+	tests := []struct {
+		name       string
+		setup      func() *Engine
+		want       []string
+		candidates []string
+		wantErr    bool
+		wantErrIs  error
+	}{
+		{
+			name: "filter pacakges",
+			setup: func() *Engine {
+				fs := &MockFileSystem{}
+				ignoreList := newTestIgnoreList(fs, slog.New(slog.NewTextHandler(io.Discard, nil)), []string{"docs"})
+				return newTestEngine("", "", fs, ignoreList)
+			},
+			candidates: []string{"nvim", "zsh", "docs", "mydocs"},
+			want:       []string{"nvim", "zsh", "mydocs"},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			engine := tc.setup()
+			filtered, err := engine.filterPackages(tc.candidates)
+			if validateErrScenario(t, tc.wantErr, err, tc.wantErrIs) {
+				return
+			}
+			if !slices.Equal(filtered, tc.want) {
+				t.Fatalf("got %v, want %v", filtered, tc.want)
+			}
+		})
+	}
+}
